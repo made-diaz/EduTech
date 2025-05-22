5c2ee96200eb342f48458e2ca896cb58e920e13c
Harakiri <madyzoon.x3@gmail.com>
2025-05-22 04:47:58 -0400

    Add files via upload
    Creacion de Proyecto

---
+++ Edutech/src/test/java/com/fullstack/Edutech/EdutechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack.Edutech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdutechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
